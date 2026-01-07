b09ea1120b9b878389ddb5c98d8d0ff74aba7e48
邓洪杰 <2210895869@qq.com>
2026-01-07 03:35:13 +0000

    chore: update 2026-01-07 03:35:12

---
+++ The-Go-Programming-Language/ch01/test1_6.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.RGBA{0x25, 0x76, 0x57, 0xff}, color.Black, color.Opaque}
+
+const (
+	whiteIndex  = 0
+	randIndex   = 1
+	blackIndex  = 2
+	opaqueIndex = 3
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 8
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, size*2+1, size*2+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*math.Pi*2; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(rand.Intn(3)+1))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
